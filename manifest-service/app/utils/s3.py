06399614dd30c8210551c5c3dcb1c7be1bb24c94
Sarang095 <sarangchafekar8788459986@gmail.com>
2025-04-02 13:01:17 +0530

    envs added

--- manifest-service/app/utils/s3.py
+++ manifest-service/app/utils/s3.py
@@ -10,12 +10,7 @@ class S3Helper:
     
     def __init__(self):
         """Initialize the S3 helper with AWS credentials."""
-        self.s3_client = boto3.client(
-            's3',
-            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-            region_name=os.getenv("AWS_REGION", "us-east-1")
-        )
+        self.s3_client = boto3.client('s3',region_name=os.getenv("AWS_REGION", "us-east-1"))
         self.cloudfront_domain = os.getenv("CLOUDFRONT_DOMAIN", "")
     
     async def upload_file_content(self, bucket, key, content, content_type="text/plain"):
--- manifest-service/app/worker.py
+++ manifest-service/app/worker.py
@@ -18,7 +18,7 @@ class SNSWorker:
     def __init__(self):
         """Initialize the SNS worker."""
         self.sqs_client = boto3.client('sqs')
-        self.queue_url = os.getenv("SQS_QUEUE_URL")
+        self.queue_url = os.getenv("SQS_QUEUE_URL", "https://sqs.us-east-1.amazonaws.com/891612545820/ABR-Streaming-Queue")
         
         self.manifest_service = ManifestService()
         
